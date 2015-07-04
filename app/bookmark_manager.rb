a8d673b0fb4500750665e2be05123805e337ad3c
Christian Miller <cwgmiller@gmail.com>
2015-07-04 12:53:53 +0100

    Notice Added for Non-Matching Passwords

--- app/bookmark_manager.rb
+++ app/bookmark_manager.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require 'sinatra/flash'
 require './app/models/link'
 require './app/data_mapper_setup'
 require 'data_mapper'
@@ -7,9 +8,9 @@ class BookmarkManager < Sinatra::Base
 
   set :views, proc { File.join(root, '..', 'views') }
   # GIVES THE CONTROLLER FILE ACCESS TO THE VIEWS FOLDER WITH ALL ERB FILES IN IT.
-
   enable :sessions
   set :session_secret, 'super secret'
+  register Sinatra::Flash
 
   get '/' do
     'Hello BookmarkManager!'
@@ -48,11 +49,16 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/users' do
-    user = User.create(email: params[:email],
+    user = User.new(email: params[:email],
                 password: params[:password],
                 password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect to('/links')
+    if user.save
+      session[:user_id] = user.id
+      redirect to('/links')
+    else
+      flash.now[:notice] = "Password and confirmation password do not match"
+      erb :'users/new'
+    end
   end
 
   helpers do
